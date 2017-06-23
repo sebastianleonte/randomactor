6ec5719a3344facb1462ba4c32ec5e7d7ba7c958
Sebastian Leonte <sleonte018@gmail.com>
2017-06-23 19:54:41 +0200

    hola

--- randomactor.py
+++ randomactor.py
@@ -6,16 +6,15 @@ lista_actores = []
 
 @app.route('/')
 def hello_world():
-    get_peoples_id(1)
-    return " ".join([x for x in lista_actores])
+    global lista_actores
+    lista_actores = []
+    lista = get_peoples_id(1)
+    return " ".join([x for x in lista])
 
 
 def get_peoples_id(page):
     actual_page = page
-    global lista_actores
-    lista_actores = []
-    listaSalida = []
-    global listita
+
     listita = []
     URL = "https://api.themoviedb.org/3/person/popular?api_key=6571f3c9bf9f6be28a99b58842d35298&language=en-EN&page=" + str(actual_page)
     try:
@@ -24,10 +23,10 @@ def get_peoples_id(page):
             print("okey!")
             d = r.json()
             print(d['total_pages'])
-            listaSalida.append(d["results"][0]["id"])
+            lista_actores.append(d["results"][0]["id"])
             for actor in d['results']:
                 lista_actores.append(actor['name'])
-                print(lista_actores)
+
 
             if actual_page < 50:
                 actual_page += 1
@@ -35,7 +34,7 @@ def get_peoples_id(page):
             # return render_template('index.html', output=listita)
     except:
         pass
-    return listaSalida
+    return print(lista_actores)
 
 
 if __name__ == '__main__':
